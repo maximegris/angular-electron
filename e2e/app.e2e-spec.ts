616a69ea0b4d3601bfcda207aca238660458ac55
Angular CLI <angular-cli@angular.io>
2017-04-02 12:36:26 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularElectronPage } from './app.po';
+
+describe('angular-electron App', () => {
+  let page: AngularElectronPage;
+
+  beforeEach(() => {
+    page = new AngularElectronPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
