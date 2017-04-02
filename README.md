7a21eb96e491c01b91b834160494681a77ef6c0f
Maxime GRIS <m.gris@emotic.fr>
2017-04-02 13:15:24 +0200

    v1.0.0

--- README.md
+++ README.md
@@ -1,28 +1,17 @@
-# AngularElectron
+[![Angular Logo](./logo-angular.jpg)](https://angular.io/) [![Electron Logo](./logo-electron.jpg)](https://electron.atom.io/)
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.0.
+# Introduction
 
-## Development server
+Bootstrap your project with Angular 4 and Electron (Typescript + SASS)
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+## To build for development
 
-## Code scaffolding
+**in a terminal window** -> npm start  
+**in another terminal window** -> npm run electron
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive/pipe/service/class/module`.
+Note: also works with yarn
 
-## Build
+## To build for production
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `-prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-Before running the tests make sure you are serving the app via `ng serve`.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+npm run build  
+npm run electron dist
---
+++ main.js
@@ -0,0 +1,66 @@
+'use strict';
+const electron = require('electron');
+// Module to control application life.
+const {app} = electron;
+// Module to create native browser window.
+const {BrowserWindow} = electron;
+
+let win;
+
+function createWindow() {
+
+    let electronScreen = electron.screen;
+    let size = electronScreen.getPrimaryDisplay().workAreaSize;
+
+    // Create the browser window.
+    win = new BrowserWindow({
+        x: 0,
+        y: 0,
+        width: size.width,
+        height: size.height
+    });
+
+    let url = 'http://localhost:4200';
+    let Args = process.argv.slice(2);
+    Args.forEach(function (val) {
+        if (val === "dist") {
+            url = 'file://' + __dirname + '/dist/index.html'
+        }
+    });
+
+    // and load the index.html of the app.
+    win.loadURL(url);
+
+    // Open the DevTools.
+    win.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
+});
