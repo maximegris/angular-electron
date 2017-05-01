7cf6a51532f589305fc488c769391c4d1a1d06b9
Maxime GRIS <maxime.gris@gmail.com>
2017-05-01 14:20:15 +0200

    update comments in dev/prod env files

--- src/environments/index.prod.ts
+++ src/environments/index.prod.ts
@@ -1,3 +1,5 @@
+// This file contains production variables. (When you work in PROD MODE)
+// This file is use by webpack. Please don't rename it and don't move it to another directory.
 export const environment = {
   production: true
 };
--- src/environments/index.ts
+++ src/environments/index.ts
@@ -1,8 +1,5 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
+// This file contains development variables. (When you work in DEV MODE)
+// This file is use by webpack. Please don't rename it and don't move it to another directory.
 export const environment = {
   production: false
 };
