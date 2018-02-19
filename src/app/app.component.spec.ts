4d3ca6e1b1fc46dea6b7d5e6a7dc09ea5d8d4d44
Sebastian Peischl <sebastian.peischl@cnc.io>
2018-02-19 21:12:37 +0100

    update dependencies and fix unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { ElectronService } from 'app/providers/electron.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -12,7 +13,10 @@ describe('AppComponent', () => {
       providers : [
         ElectronService
       ],
-      imports: [RouterTestingModule]
+      imports: [
+        RouterTestingModule,
+        TranslateModule.forRoot()
+      ]
     }).compileComponents();
   }));
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import 'zone.js/dist/zone-mix';
 import 'reflect-metadata';
-import 'polyfills';
+import '../polyfills';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -15,7 +15,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { ElectronService } from './providers/electron.service';
 
-import { WebviewDirective } from 'app/directives/webview.directive';
+import { WebviewDirective } from './directives/webview.directive';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
