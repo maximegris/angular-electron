7863aa97d2fa37e7d1e0ef66a29ed95876e49260
Maxime GRIS <m.gris@emotic.fr>
2018-02-25 22:15:09 +0100

    fix/ TranslateModule test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { ElectronService } from 'app/providers/electron.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { ElectronService } from './providers/electron.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
