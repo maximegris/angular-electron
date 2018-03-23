d78203f76029069a25285741171487d3a2f4c361
Shal Jain <sjain@knectiq.com>
2018-03-23 11:41:35 -0500

    Including electron to eliminate Electron not found err sg

--- main.ts
+++ main.ts
@@ -27,7 +27,7 @@ function createWindow() {
 
   if (serve) {
     require('electron-reload')(__dirname, {
-    });
+     electron: require(`${__dirname}/node_modules/electron`)});
     win.loadURL('http://localhost:4200');
   } else {
     win.loadURL(url.format({
