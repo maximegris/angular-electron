6bd044e4597dc5099819ac1a495c5b88f1fe6de9
whyboris <byakubchik@forbes.com>
2018-04-07 14:24:39 -0400

    provide webFrame access

--- src/app/providers/electron.service.ts
+++ src/app/providers/electron.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 // If you import a module but never use any of the imported values other than as TypeScript types,
 // the resulting javascript file will look as if you never imported the module at all.
-import { ipcRenderer } from 'electron';
+import { ipcRenderer, webFrame } from 'electron';
 import * as childProcess from 'child_process';
 
 @Injectable()
@@ -10,12 +10,14 @@ export class ElectronService {
 
   ipcRenderer: typeof ipcRenderer;
   childProcess: typeof childProcess;
+  webFrame: typeof webFrame;
 
   constructor() {
     // Conditional imports
     if (this.isElectron()) {
       this.ipcRenderer = window.require('electron').ipcRenderer;
       this.childProcess = window.require('child_process');
+      this.webFrame = window.require('electron').webFrame;
     }
   }
 
