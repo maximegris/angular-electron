84f08c86a6243fc1b3d03fc3e718fef152bdd77b
teminal-ninja <stuart@ninjatech.marketing>
2018-06-29 13:42:21 +0200

    non

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import 'zone.js/dist/zone-mix';
 import 'reflect-metadata';
 import '../polyfills';
 import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -45,6 +46,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   ],
   imports: [
     BrowserModule,
+    // BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
