0220d956251fa68a9b1f46bada3e59999a1b8892
terminal-ninja <stuart@ninjatech.marketing>
2019-03-15 09:25:38 +0200

    non

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -70,7 +70,9 @@ export class NavbarComponent implements OnInit {
 
   async logout() {
     await this.download.deleteStorage()
-    await this.download.deleteTmp()
+    if (process.platform === 'darwin') {
+      await this.download.deleteTmp()
+    }
     /* Delete temp, not working on Win, disabled
     // await this.download.deleteTmp()
     */
