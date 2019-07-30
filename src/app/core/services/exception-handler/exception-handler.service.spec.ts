a6905840b115e1ce13868069bc125824f8c534b3
Arun Redhu <arun.redhu05@gmail.com>
2019-07-30 22:44:03 +0530

    [UPDATE]: Exception handler test bug fixes

--- src/app/core/services/exception-handler/exception-handler.service.spec.ts
+++ src/app/core/services/exception-handler/exception-handler.service.spec.ts
@@ -1,3 +1,4 @@
+import { ErrorHandler } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 import { ExceptionHandlerService } from './exception-handler.service';
@@ -6,7 +7,7 @@ describe('ExceptionHandlerService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: ExceptionHandlerService = TestBed.get(ExceptionHandlerService);
+    const service: ExceptionHandlerService = TestBed.get(ErrorHandler);
     expect(service).toBeTruthy();
   });
 });
