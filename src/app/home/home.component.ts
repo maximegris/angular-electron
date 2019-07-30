8097cad5be1549135528585b487d016b54c385a3
Arun Redhu <arun.redhu05@gmail.com>
2019-07-30 22:41:34 +0530

    [ADD]: Angular error handler integrated to handle the errors/exceptions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,5 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {
-    throw new Error('my error');
-  }
+  ngOnInit() {}
 }
