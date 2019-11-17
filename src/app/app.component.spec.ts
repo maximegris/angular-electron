2f64f37e885e55cad0aa74020a40d2e50e44c4c2
Yuri Cherepanov <jurij.cherepanov@egamings.com>
2019-11-18 01:47:30 +0200

    eslint-migration - fix removed translation import

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { TranslateModule } from '@ngx-translate/core';
 import { ElectronService } from './core/services';
 
 describe('AppComponent', () => {
