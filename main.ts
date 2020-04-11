7c5c43bad76b34142d77622cb2931d0845271039
Maxime GRIS <mgris@sigma.fr>
2020-04-11 21:47:57 +0200

    ref/ set allowRendererProcessReuse to true

--- main.ts
+++ main.ts
@@ -53,6 +53,8 @@ function createWindow(): BrowserWindow {
 
 try {
 
+  app.allowRendererProcessReuse = true;
+
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
