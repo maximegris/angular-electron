4c0c1696840d65a1a07a411b03804fadaefab956
Srinivas <bsrinivasguptha@gmail.com>
2020-04-14 16:52:46 +0530

    Transparent background issue fix for Linux

--- main.ts
+++ main.ts
@@ -58,7 +58,8 @@ try {
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow);
+  // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
+  app.on('ready', () => setTimeout(createWindow, 400));
 
   // Quit when all windows are closed.
   app.on('window-all-closed', () => {
