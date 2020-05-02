acc62d9bfed0b6c0e8192ded1cc158b74587ebf4
Maxime GRIS <mgris@sigma.fr>
2020-05-02 13:27:48 +0200

    feat/ VSCode : add Debugging config for man process (#465) + Karma

--- main.ts
+++ main.ts
@@ -24,20 +24,21 @@ function createWindow(): BrowserWindow {
   });
 
   if (serve) {
+
+    require('devtron').install();
+    win.webContents.openDevTools();
+
     require('electron-reload')(__dirname, {
       electron: require(`${__dirname}/node_modules/electron`)
     });
     win.loadURL('http://localhost:4200');
+
   } else {
     win.loadURL(url.format({
       pathname: path.join(__dirname, 'dist/index.html'),
       protocol: 'file:',
       slashes: true
     }));
-  }
-
-  if (serve) {
-    win.webContents.openDevTools();
   }
 
   // Emitted when the window is closed.
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -24,9 +24,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['AngularElectron'],
-    singleRun: true,
     customLaunchers: {
       AngularElectron: {
         base: 'Electron',
