e3a4e01a0e320a7c1ddd7c70d4b8aa997dcac005
Maxime GRIS <mgris@sigma.fr>
2020-08-15 12:38:59 +0200

    ref/ clean custom webpack target ('electron-renderer' or 'web')

--- angular.webpack.js
+++ angular.webpack.js
@@ -4,17 +4,17 @@
 
 module.exports = (config, options) => {
     config.target = 'electron-renderer';
-    if (options.customWebpackConfig.target) {
-        config.target = options.customWebpackConfig.target;
-    } else if (options.fileReplacements) {
+
+
+    if (options.fileReplacements) {
         for(let fileReplacement of options.fileReplacements) {
             if (fileReplacement.replace !== 'src/environments/environment.ts') {
                 continue;
             }
 
             let fileReplacementParts = fileReplacement['with'].split('.');
-            if (['dev', 'prod', 'test', 'electron-renderer'].indexOf(fileReplacementParts[1]) < 0) {
-                config.target = fileReplacementParts[1];
+            if (fileReplacementParts.length > 1 && ['web'].indexOf(fileReplacementParts[1]) >= 0) {
+                config.target = 'web';
             }
             break;
         }
