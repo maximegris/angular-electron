9933622bda3cedf378966c07309a03fb05dd4788
Maxime GRIS <maxime.gris@gmail.com>
2020-11-15 11:21:07 +0100

    misc/ remove old code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,3 @@
-import 'reflect-metadata';
-import '../polyfills';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
