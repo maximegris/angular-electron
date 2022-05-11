392de532dbafd767a9fd99a7382982c3e62c989a
Ken Riley <kriley@uvangel.com>
2022-05-11 17:06:15 -0500

    Progress bars in place for Parts Status. Beginnings of Event Table

--- src/app/detail/detail.module.ts
+++ src/app/detail/detail.module.ts
@@ -9,9 +9,11 @@ import { DataViewComponent } from './data-view/data-view.component';
 import { DynamicTreatmentViewComponent } from './dynamic-treatment-view/dynamic-treatment-view.component';
 import { InstallServiceViewComponent } from './install-service-view/install-service-view.component';
 import { MatCardModule } from '@angular/material/card'
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [DetailComponent, DataViewComponent, DynamicTreatmentViewComponent, InstallServiceViewComponent],
-  imports: [CommonModule, SharedModule, DetailRoutingModule, MatCardModule]
+  imports: [CommonModule, SharedModule, DetailRoutingModule, MatCardModule, MatProgressBarModule, MatTableModule]
 })
 export class DetailModule {}
--- src/app/detail/dynamic-treatment-view/dynamic-treatment-view.component.ts
+++ src/app/detail/dynamic-treatment-view/dynamic-treatment-view.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dynamic-treatment-view',
   templateUrl: './dynamic-treatment-view.component.html',
-  styleUrls: ['./dynamic-treatment-view.component.scss']
+  styleUrls: ['./dynamic-treatment-view.component.scss'],
 })
+
 export class DynamicTreatmentViewComponent implements OnInit {
 
   constructor() { }
@@ -12,4 +13,23 @@ export class DynamicTreatmentViewComponent implements OnInit {
   ngOnInit(): void {
   }
 
+}
+
+export interface DeviceEvent {
+  item: string,
+  eventType: string,
+  date: string
+}
+
+const EVENT_DATA: DeviceEvent[] =
+  [
+    { item: 'Lamp', eventType: 'Removed', date: '12-22-2021' },
+    { item: 'Filter', eventType: 'Detected new', date: '12-22-2021' },
+    { item: 'Door', eventType: 'Opened', date: '12-22-2021' },
+    { item: 'Filter', eventType: 'Detected new', date: '12-22-2021' },
+  ];
+
+export class EventTable {
+  displayedColumns: string[] = ['item', 'eventType', 'date'];
+  dataSource = EVENT_DATA;
 }
