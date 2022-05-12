c3fd1f66a1b4122c3974a6cc2e4578efea9f4611
Ken Riley <kriley@uvangel.com>
2022-05-12 12:22:21 -0500

    Fixed styling of buttons

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,9 +6,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule, ReactiveFormsModule, FormsModule]
+  imports: [CommonModule, SharedModule, HomeRoutingModule, ReactiveFormsModule, FormsModule, MatSelectModule, MatButtonModule]
 })
 export class HomeModule {}
