152d3aba5d452e148f866e50704cfc1a3adf2bfa
Luke Platz <lplatz@uvangel.com>
2022-05-12 12:29:06 -0500

    switch to import mattablemodule

--- src/app/detail/detail.module.ts
+++ src/app/detail/detail.module.ts
@@ -10,11 +10,11 @@ import { DynamicTreatmentViewComponent } from './dynamic-treatment-view/dynamic-
 import { InstallServiceViewComponent } from './install-service-view/install-service-view.component';
 import { MatCardModule } from '@angular/material/card'
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatTable } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { UvaEventTable } from '../shared/components/uva-event-table/uva-event-table.component';
 
 @NgModule({
-  declarations: [DetailComponent, DataViewComponent, DynamicTreatmentViewComponent, InstallServiceViewComponent, UvaEventTable, MatTable],
-  imports: [CommonModule, SharedModule, DetailRoutingModule, MatCardModule, MatProgressBarModule]
+  declarations: [DetailComponent, DataViewComponent, DynamicTreatmentViewComponent, InstallServiceViewComponent, UvaEventTable],
+  imports: [CommonModule, SharedModule, DetailRoutingModule, MatCardModule, MatProgressBarModule, MatTableModule]
 })
 export class DetailModule {}
