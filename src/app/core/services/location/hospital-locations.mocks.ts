3e3474400b6fed1a7032e843377bcbcd68cd4ba0
Stefan Radacovsky <stefan.radacovsky@softec.cz>
2022-05-20 17:10:10 +0200

    New location mocks

---
+++ src/app/core/services/location/hospital-locations.mocks.ts
@@ -0,0 +1,45 @@
+export const locationMocks: Record<string, any> = {};
+
+locationMocks['626a4afeddd89a00091c2420'] =
+    {
+        "id": "626a4afeddd89a00091c2420",
+        "name": "UV Angel Hospital",
+        "type": "BUILDING",
+        "description": null,
+        "tags": [],
+        "immediateSublocations": [
+            {
+                "id": "626a4bb1ddd89a00091c2421",
+                "name": "Underground Garages",
+                "type": "FLOOR",
+                "description": null,
+                "tags": [],
+                "__typename": "Location"
+            },
+            {
+                "id": "626a4bc4ddd89a00091c2422",
+                "name": "Ground Floor",
+                "type": "FLOOR",
+                "description": null,
+                "tags": [],
+                "__typename": "Location"
+            },
+            {
+                "id": "626a4bd6ddd89a00091c2423",
+                "name": "Floor 2",
+                "type": "FLOOR",
+                "description": null,
+                "tags": [],
+                "__typename": "Location"
+            },
+            {
+                "id": "626a4be8ddd89a00091c2424",
+                "name": "Top Floor Outside",
+                "type": "FLOOR",
+                "description": null,
+                "tags": [],
+                "__typename": "Location"
+            }
+        ],
+        "__typename": "Location"
+    };
