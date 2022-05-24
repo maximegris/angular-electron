6e036d9266f176349f95f3ce71c61e06520814f7
Ken Riley <kriley@uvangel.com>
2022-05-24 18:32:04 -0500

    refactor-environment-service

--- src/app/shared/components/uva-device-mode/uva-device-mode.component.ts
+++ src/app/shared/components/uva-device-mode/uva-device-mode.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnChanges, SimpleChange, OnInit, SimpleChanges } from '@angular/core';
-import { RollingEnvironmentalData } from '../../../core/services/service.model';
+import { Component, OnChanges, SimpleChange, OnInit, SimpleChanges, Input } from '@angular/core';
+import { Device, RollingEnvironmentalData } from '../../../core/services/service.model';
 import { EnvironmentData, EnvironmentService } from '../../../core/services/environment/environment.service';
-import { DeviceService,  } from '../../../core/services/device/device.service';
-import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
+import { AbstractComponent } from '../../../core/abstract.component';
+import { DeviceService } from '../../../core/services/device/device.service';
+import { BehaviorSubject, Subject, takeUntil, Subscription } from 'rxjs';
 import { isThisSecond } from 'date-fns';
 
 enum DeviceMode {
@@ -17,7 +18,9 @@ enum DeviceMode {
   templateUrl: './uva-device-mode.component.html',
   styleUrls: ['./uva-device-mode.component.scss']
 })
-export class UvaDeviceModeComponent implements OnInit, OnChanges {
+export class UvaDeviceModeComponent extends AbstractComponent implements OnInit {
+  device: Device
+  deviceSubscription: Subscription;
   public totalAq: number;
   public deviceMode: DeviceMode;
   public deviceImage: string;
@@ -30,29 +33,47 @@ export class UvaDeviceModeComponent implements OnInit, OnChanges {
   public isManualMode: boolean = false;
   unsubscribeManualMode$: Subject<boolean> = new Subject();
 
-  constructor(private manualEnviroService: EnvironmentService) { }
-
-  ngOnInit(): void {
-    this.manualEnviroService.isManualMode
-      .pipe(takeUntil(this.unsubscribeManualMode$))
-      .subscribe(isManualMode => {
-        if(isManualMode) {
-          this.manualEnviroService.environmentData
-          .pipe(takeUntil(this.unsubscribe$))
-          .subscribe(environmentData => {
-            this.totalAq = environmentData.total;
-            this.getDeviceMode();
-          });
-        } else {
-          console.log('In UvaDeviceModeComponent: Need to grab data from device service')
-        }
-  
-
-      })
+  constructor(private envService: EnvironmentService) {
+    super();
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(`UvaDeviceModeComponent.ngOnChanges ${JSON.stringify(changes, null, 2)}`)
+  ngOnInit(): void {
+    this.envService.currentDevice$.pipe(
+      takeUntil(this.destroyed$)
+    ).subscribe(d => {
+      let settingNewDevice: boolean = false;
+      // if we are setting a device or changing devices, set up new subscription
+      if (!this.device || (d.name && this.device?.name && d.name !== this.device?.name)) {
+        settingNewDevice = true;
+      }
+      this.device = d;
+      if (settingNewDevice) {
+        this.deviceSubscription?.unsubscribe();
+        this.deviceSubscription = this.device.environmentalData
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe(envData => {
+
+            this.totalAq = envData.total.data[59].value;
+            this.getDeviceMode();
+          });
+      }
+    });
+
+    this.envService.isManualMode
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(isManualMode => {
+        console.log(`uva-device-panel manual mode = ${isManualMode}`)
+        this.device.useOverrideValues = isManualMode
+      });
+    this.envService.environmentData
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(environmentalData => {
+        console.log('setting new env data overrides', environmentalData)
+        this.device.setEnvironmentalOverrideData(environmentalData)
+        this.getDeviceMode()
+      })
+
+
   }
 
   getDeviceMode(): void {
