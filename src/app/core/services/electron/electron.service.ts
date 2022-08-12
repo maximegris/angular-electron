e1da5b51669f8cba5b048e38df7283aa914f95c0
Maxime GRIS <maxime.gris@gmail.com>
2022-08-12 13:07:27 +0200

    refactor/ build electron archive as asar

--- src/app/core/services/electron/electron.service.ts
+++ src/app/core/services/electron/electron.service.ts
@@ -21,8 +21,20 @@ export class ElectronService {
       this.ipcRenderer = window.require('electron').ipcRenderer;
       this.webFrame = window.require('electron').webFrame;
 
-      this.childProcess = window.require('child_process');
       this.fs = window.require('fs');
+
+      this.childProcess = window.require('child_process');
+      this.childProcess.exec('node -v', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.error(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout:\n${stdout}`);
+      });
 
       // Notes :
       // * A NodeJS's dependency imported with 'window.require' MUST BE present in `dependencies` of both `app/package.json`
