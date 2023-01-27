8a7e6ece04af8f71b14641fe6000cacdf0c33bc3
jacek Byczkiewicz <jck53416@gmail.com>
2023-01-28 00:06:28 +0100

    Add material angular dependencty

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void {
     console.log('HomeComponent INIT');
