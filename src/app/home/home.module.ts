2213ae4657b64bb642c7c45e75059471b46e4b3b
jacek Byczkiewicz <jck53416@gmail.com>
2023-01-28 18:40:38 +0100

    Add material icons

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule]
+  imports: [CommonModule, SharedModule, MatIconModule]
 })
 export class HomeModule { }
