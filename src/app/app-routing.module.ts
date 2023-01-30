a8ed429bedc9423e262e79d09aa03f1e8af11e3d
zdhsoft <zdhsoft@163.com>
2023-01-30 16:54:58 +0800

    升级angular和electron

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes, {}),
     HomeRoutingModule,
     DetailRoutingModule
   ],
